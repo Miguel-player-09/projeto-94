60b822c970e9be76dc4cd7cc6ac646af89594b82
Miguel-player-09 <121067934+Miguel-player-09@users.noreply.github.com>
2023-05-11 20:59:52 -0300

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDKZ0rm9lWsLtUtcxSd6Jyb4j5uCpDhRBc",
+    authDomain: "chat-player-09.firebaseapp.com",
+    databaseURL: "https://chat-player-09-default-rtdb.firebaseio.com",
+    projectId: "chat-player-09",
+    storageBucket: "chat-player-09.appspot.com",
+    messagingSenderId: "265479108872",
+    appId: "1:265479108872:web:26f9fd888b8adbc7fe14bd"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
